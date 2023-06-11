340a967d317abbcc75ecd27f9632ed4917fe8b56
therockstarind <rohitkumarrohit96@gmail.com>
2023-06-11 19:23:50 +0530

    feat: Redesign Gallery page and there componets

--- src/components/OneDrive/PhotoCard.tsx
+++ src/components/OneDrive/PhotoCard.tsx
@@ -2,38 +2,45 @@ import React, { useEffect, useState } from 'react';
 import { fetchMediaFiles, OnedriveFile } from '@lib/onedriveApi';
 import { RiArrowLeftSLine, RiArrowRightSLine, RiCloseLine } from 'react-icons/ri';
 
-const MediaCard: React.FC = () => {
+const Skeleton: React.FC = () => (
+  <div className="w-full h-48 md:h-64 bg-gray-300 animate-pulse rounded-2xl group-hover:scale-105 transition-transform overflow-hidden" />
+);
+
+const PhotoCard: React.FC = () => {
   const [files, setFiles] = useState<OnedriveFile[]>([]);
-  const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
+  const [selectedImageIndex, setSelectedImageIndex] = useState<number | null>(null);
   const [isSliding, setIsSliding] = useState<boolean>(false);
   const [slideStartX, setSlideStartX] = useState<number | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const getMediaFiles = async () => {
       const mediaFiles = await fetchMediaFiles();
-      setFiles(mediaFiles);
+      const imageFiles = mediaFiles.filter(file => file.file.mimeType.startsWith('image/'));
+      setFiles(imageFiles);
+      setLoading(false);
     };
 
     getMediaFiles();
   }, []);
 
-  const handleMediaClick = (index: number) => {
-    setSelectedIndex(index);
+  const handleImageClick = (index: number) => {
+    setSelectedImageIndex(index);
   };
 
   const handlePopupClose = () => {
-    setSelectedIndex(null);
+    setSelectedImageIndex(null);
   };
 
-  const handleNextMedia = () => {
-    if (selectedIndex !== null && selectedIndex < files.length - 1) {
-      setSelectedIndex(selectedIndex + 1);
+  const handleNextImage = () => {
+    if (selectedImageIndex !== null && selectedImageIndex < files.length - 1) {
+      setSelectedImageIndex(selectedImageIndex + 1);
     }
   };
 
-  const handlePreviousMedia = () => {
-    if (selectedIndex !== null && selectedIndex > 0) {
-      setSelectedIndex(selectedIndex - 1);
+  const handlePreviousImage = () => {
+    if (selectedImageIndex !== null && selectedImageIndex > 0) {
+      setSelectedImageIndex(selectedImageIndex - 1);
     }
   };
 
@@ -50,10 +57,10 @@ const MediaCard: React.FC = () => {
 
     if (deltaX > sensitivity) {
       setIsSliding(false);
-      handlePreviousMedia();
+      handlePreviousImage();
     } else if (deltaX < -sensitivity) {
       setIsSliding(false);
-      handleNextMedia();
+      handleNextImage();
     }
   };
 
@@ -72,45 +79,38 @@ const MediaCard: React.FC = () => {
         <div
           key={file.id}
           className="relative shadow dark:shadow-gray-600 p-2 rounded-2xl group transition-[opacity,transform] duration-100"
-          onClick={() => handleMediaClick(index)}
+          onClick={() => handleImageClick(index)}
         >
-          {file.file.mimeType.startsWith('image/') ? (
+          {loading ? (
+            <Skeleton />
+          ) : (
             <img
               src={file.thumbnail || file['@content.downloadUrl']}
               alt={file.name}
               className="w-full h-48 md:h-64 object-cover rounded-2xl group-hover:scale-105 transition-transform overflow-hidden"
             />
-          ) : (
-            <div className="relative w-full h-48 md:h-64 rounded-2xl group-hover:scale-105 transition-transform overflow-hidden">
-              <video src={file.thumbnail || file['@content.downloadUrl']} className="w-full h-full object-cover rounded-2xl" />
-              <div className="absolute inset-0 flex items-center justify-center">
-                <svg className="w-12 h-12 text-white opacity-80 transition-opacity duration-200 group-hover:opacity-100" viewBox="0 0 24 24">
-                  <path fill="currentColor" d="M8 5v14l11-7z" />
-                </svg>
-              </div>
-            </div>
           )}
         </div>
       ))}
 
-      {selectedIndex !== null && (
+      {selectedImageIndex !== null && (
         <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-75 z-50">
-          {files[selectedIndex].file.mimeType.startsWith('image/') ? (
-            <img src={files[selectedIndex]['@content.downloadUrl']} alt="Full Image" className="max-w-full max-h-full" />
-          ) : (
-            <video src={files[selectedIndex]['@content.downloadUrl']} className="max-w-full max-h-full" controls />
-          )}
+          <img
+            src={files[selectedImageIndex]['@content.downloadUrl']}
+            alt="Full Image"
+            className="max-w-full max-h-full"
+          />
           <button
             className="absolute top-1/2 left-4 bg-white dark:bg-gray-600 text-black dark:text-white rounded-full p-2 transform -translate-y-1/2"
-            onClick={handlePreviousMedia}
-            disabled={selectedIndex === 0}
+            onClick={handlePreviousImage}
+            disabled={selectedImageIndex === 0}
           >
             <RiArrowLeftSLine size={24} />
           </button>
           <button
             className="absolute top-1/2 right-4 bg-white dark:bg-gray-600 text-black dark:text-white rounded-full p-2 transform -translate-y-1/2"
-            onClick={handleNextMedia}
-            disabled={selectedIndex === files.length - 1}
+            onClick={handleNextImage}
+            disabled={selectedImageIndex === files.length - 1}
           >
             <RiArrowRightSLine size={24} />
           </button>
@@ -126,4 +126,4 @@ const MediaCard: React.FC = () => {
   );
 };
 
-export default MediaCard;
+export default PhotoCard;
--- src/components/OneDrive/VideoCard.tsx
+++ src/components/OneDrive/VideoCard.tsx
@@ -1,46 +1,129 @@
-import React, { useRef, useEffect, useState } from "react";
-import { motion } from "framer-motion";
-import { fromLeftChildren } from "@content/FramerMotionVariants";
-import Plyr from "plyr";
-import "plyr/dist/plyr.css";
+import React, { useEffect, useState } from 'react';
+import { fetchMediaFiles, OnedriveFile } from '@lib/onedriveApi';
+import { RiArrowLeftSLine, RiArrowRightSLine, RiCloseLine } from 'react-icons/ri';
 
-export default function VideoCard({
-  url,
-  isControls = true,
-}: {
-  url: string;
-  isControls?: boolean;
-}) {
-  const videoRef = useRef<HTMLVideoElement>(null);
-  const [, setPlayer] = useState<Plyr>();
+const VideoCard: React.FC = () => {
+  const [files, setFiles] = useState<OnedriveFile[]>([]);
+  const [selectedVideoIndex, setSelectedVideoIndex] = useState<number | null>(null);
+  const [isSliding, setIsSliding] = useState<boolean>(false);
+  const [slideStartX, setSlideStartX] = useState<number | null>(null);
 
   useEffect(() => {
-    if (videoRef.current) {
-      const plyrPlayer = new Plyr(videoRef.current);
-      setPlayer(plyrPlayer);
+    const getMediaFiles = async () => {
+      const mediaFiles = await fetchMediaFiles();
+      const videoFiles = mediaFiles.filter(file => file.file.mimeType.startsWith('video/'));
+      setFiles(videoFiles);
+    };
+
+    getMediaFiles();
+  }, []);
+
+  const handleVideoClick = (index: number) => {
+    setSelectedVideoIndex(index);
+  };
+
+  const handlePopupClose = () => {
+    setSelectedVideoIndex(null);
+  };
+
+  const handleNextVideo = () => {
+    if (selectedVideoIndex !== null && selectedVideoIndex < files.length - 1) {
+      setSelectedVideoIndex(selectedVideoIndex + 1);
     }
-  }, [videoRef]);
+  };
+
+  const handlePreviousVideo = () => {
+    if (selectedVideoIndex !== null && selectedVideoIndex > 0) {
+      setSelectedVideoIndex(selectedVideoIndex - 1);
+    }
+  };
+
+  const handleTouchStart = (e: React.TouchEvent<HTMLDivElement>) => {
+    setIsSliding(true);
+    setSlideStartX(e.touches[0].clientX);
+  };
+
+  const handleTouchMove = (e: React.TouchEvent<HTMLDivElement>) => {
+    if (!isSliding) return;
+    const currentX = e.touches[0].clientX;
+    const deltaX = currentX - (slideStartX || 0);
+    const sensitivity = 50;
+
+    if (deltaX > sensitivity) {
+      setIsSliding(false);
+      handlePreviousVideo();
+    } else if (deltaX < -sensitivity) {
+      setIsSliding(false);
+      handleNextVideo();
+    }
+  };
+
+  const handleTouchEnd = () => {
+    setIsSliding(false);
+  };
 
   return (
-    <motion.div
-      variants={fromLeftChildren}
-      className="relative rounded-sm group transition-[opacity,transform] duration-100 z-1"
+    <div
+      className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-5 gap-4"
+      onTouchStart={handleTouchStart}
+      onTouchMove={handleTouchMove}
+      onTouchEnd={handleTouchEnd}
     >
-      <div
-        className={`${
-          isControls ? "aspect-video" : "aspect-square"
-        } relative rounded-sm overflow-hidden`}
-      >
-        <video
-          ref={videoRef}
-          className={`plyr__video-embed ${
-            isControls ? "aspect-video" : "aspect-square"
-          } rounded-lg transition-transform`}
-          controls={isControls}
-        >
-          <source src={url} />
-        </video>
-      </div>
-    </motion.div>
+      {files.map((file, index) => (
+        <div
+          key={file.id}
+          className="relative shadow dark:shadow-gray-600 p-2 rounded-2xl group transition-[opacity,transform] duration-100"
+          onClick={() => handleVideoClick(index)}
+        >             
+          <video
+            src={file.thumbnail || file["@content.downloadUrl"]}
+            className="w-full h-48 md:h-64 object-cover rounded-2xl group-hover:scale-105 transition-transform overflow-hidden"
+          />
+          <div className="absolute inset-0 flex items-center justify-center">
+            <svg
+              className="w-12 h-12 text-white opacity-80 transition-opacity duration-200 group-hover:opacity-100"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill="currentColor"
+                d="M8 5v14l11-7z"
+              />
+            </svg>
+          </div>
+        </div>       
+      ))}
+
+      {selectedVideoIndex !== null && (
+        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-75 z-50">
+          <video
+            src={files[selectedVideoIndex]["@content.downloadUrl"]}
+            className="max-w-full max-h-full"
+            controls
+          />
+          <button
+            className="absolute top-1/2 left-4 bg-white dark:bg-gray-600 text-black dark:text-white rounded-full p-2 transform -translate-y-1/2"
+            onClick={handlePreviousVideo}
+            disabled={selectedVideoIndex === 0}
+          >
+            <RiArrowLeftSLine size={24} />
+          </button>
+          <button
+            className="absolute top-1/2 right-4 bg-white dark:bg-gray-600 text-black dark:text-white rounded-full p-2 transform -translate-y-1/2"
+            onClick={handleNextVideo}
+            disabled={selectedVideoIndex === files.length - 1}
+          >
+            <RiArrowRightSLine size={24} />
+          </button>
+          <button
+            className="absolute top-4 right-4 bg-white dark:bg-gray-600 text-black dark:text-white rounded-full p-2"
+            onClick={handlePopupClose}
+          >
+            <RiCloseLine size={24} />
+          </button>
+        </div>
+      )}
+    </div>
   );
-}
+};
+
+export default VideoCard;
