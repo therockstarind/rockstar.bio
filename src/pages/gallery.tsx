968b6692cf2841dba593be766871ad564cef2082
therockstarind <rohitkumarrohit96@gmail.com>
2023-06-18 12:30:10 +0530

    feat: add title to gallery page and fix small css

--- src/pages/gallery.tsx
+++ src/pages/gallery.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import Seo from "@/components/Seo";
 import GalleryCard from "@/components/OneDrive/GalleryCard";
+import { motion } from "framer-motion";
+import { FadeContainer, opacityVariant } from "@/content/FramerMotionVariants";
+import AnimatedDiv from "@/components/FramerMotion/AnimatedDiv";
 
-export default function Gallery({
-}: {}) {
+export default function Gallery({}: {}) {
+  
   return (
     <>
       <Seo
@@ -14,7 +17,31 @@ export default function Gallery({
         ogImage="https://rockstar.bio/og-img/gallery.png" 
       />
         <section className="pageGallery font-inter max-w-6xl">
-          <GalleryCard />
+          <div>
+          <motion.h3
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            variants={opacityVariant}
+            className="text-3xl sm:text-4xl capitalize font-bold text-neutral-900 dark:text-neutral-200"
+          >
+            Photos & Videos
+          </motion.h3>
+          <motion.h3
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            variants={opacityVariant}
+            className="text-sm"
+          >
+            Note: all the content in the original quality so must be need better internet to consume it.
+          </motion.h3>
+          <AnimatedDiv
+              variants={FadeContainer}
+            >
+                <GalleryCard />
+            </AnimatedDiv>
+          </div>
         </section>
     </>
   );
